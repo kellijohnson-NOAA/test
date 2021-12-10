24f612eb5f1094ac71428975a52f80445ed4fc30
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-12-10 11:49:21 -0800

    Change to units

--- R/test.R
+++ R/test.R
@@ -1,7 +1,12 @@
 #' Function
-#' @param good good argument
 
-test <- function(good) {
-  print(good)
-  nwfscSurvey::createMatrix()
+test <- function() {
+  x <- 1:3
+  class(x)
+  units(x) <- units::as_units("m/s")
+  class(x)
+  y = 2:5
+  a <- units::set_units(1:3, m/s)
+  units(a) <- units::make_units(km/h)
+  a
 }
